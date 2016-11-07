91f2370aadb2106fc71ff9b3fb06e8bd3b2299a7
franzmoro <franzmoro88@gmail.com>
2016-11-07 01:11:02 +0000

    :art: move dismissKeyBoard
    to react modules injected in actions

--- app/react_modules.js
+++ app/react_modules.js
@@ -1,10 +1,14 @@
+import dismissKeyBoard from 'react-native/Libraries/Utilities/dismissKeyboard.js';
+
 module.exports = {
   ios: {
     alert:require('react-native').AlertIOS,
+    dismissKeyBoard
     // although the "Alert" module can be used also for iOS,
     // this is to show how different modules can be handled
   },
   android: {
-    alert:require('react-native').Alert
+    alert:require('react-native').Alert,
+    dismissKeyBoard
   }
 };
